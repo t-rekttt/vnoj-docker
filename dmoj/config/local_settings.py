3757adb35a6b9e6e511390a5e1994e753c8be1cf
T-Rekt <t.to.the.rekt@gmail.com>
2023-04-07 17:08:05 +0700

    Update local_settings.py

--- dmoj/config/local_settings.py
+++ dmoj/config/local_settings.py
@@ -17,7 +17,7 @@ HOST = os.environ.get('HOST', 'localhost')
 
 # Uncomment and set to the domain names this site is intended to serve.
 # You must do this once you set DEBUG to False.
-ALLOWED_HOSTS = [HOST]
+ALLOWED_HOSTS = HOST.split(',')
 
 # Optional apps that DMOJ can make use of.
 INSTALLED_APPS += ()
